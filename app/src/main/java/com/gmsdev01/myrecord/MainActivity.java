8bac50c2e902448e79c98183bba3473a89bec80c
Adi T <gmsdev01@gmail.com>
2018-09-03 18:02:12 +0300

    redone

--- app/src/main/java/com/gmsdev01/myrecord/MainActivity.java
+++ app/src/main/java/com/gmsdev01/myrecord/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent stopIntent=new Intent(getBaseContext(), RecService.class);
-                startService(stopIntent);
+                stopService(stopIntent);
 
             }
         });
--- app/src/main/java/com/gmsdev01/myrecord/RecService.java
+++ app/src/main/java/com/gmsdev01/myrecord/RecService.java
@@ -23,15 +23,35 @@ public class RecService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.v(LOG_TAG, "Service started ");
-        Toast.makeText(this, "service has started",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "service has started", Toast.LENGTH_LONG).show();
+
+        Thread t = new Thread(r);
+        t.start();
 
         return START_NOT_STICKY;
-
     }
+
 
     @Override
     public void onDestroy() {
         Log.v(LOG_TAG, "Service stopped ");
-        Toast.makeText(this, "service has stopped",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "service has stopped", Toast.LENGTH_LONG).show();
     }
+
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            Log.v(LOG_TAG, Thread.currentThread().getId() + ": Thread started ");
+            for (int i = 0; i < 10; i++) {
+                Log.v(LOG_TAG, Thread.currentThread().getId() + ": " + Integer.toString(i));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.v(LOG_TAG, Thread.currentThread().getId() + ": Thread finished ");
+
+        }
+    };
 }
